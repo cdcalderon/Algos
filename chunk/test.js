4ffc130f90c04cb39fe7de0ce7a783cc932cbee3
Carlos Calderon <cdcalderon@gmail.com>
2022-04-21 19:13:27 -0500

    fix tests

--- chunk/test.js
+++ chunk/test.js
@@ -5,12 +5,15 @@ test('function chunk exists', () => {
 })
 
 test('chunk divides an array of 10 elements with chunk size 2', () => {
-    const arr = [1, 2, 3, 4]
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     const chunked = chunk(arr, 2)
 
     expect(chunked).toEqual([
         [1, 2],
         [3, 4],
+        [5, 6],
+        [7, 8],
+        [9, 10],
     ])
 })
 
