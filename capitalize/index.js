b0246d67cd95d377942fa060d677c930c49c421c
Carlos Calderon <cdcalderon@gmail.com>
2022-04-25 15:50:53 -0500

    implement solution one for capitalize ploblem

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,6 +7,17 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    return capitalizeSolutionOne(str)
+}
+
+function capitalizeSolutionOne(str) {
+    return str
+        .split(' ')
+        .map((w) => `${w[0].toUpperCase()}${w.slice(1)}`)
+        .join(' ')
+}
+
+function capitalizeSolutionTwo(str) {}
 
 module.exports = capitalize
