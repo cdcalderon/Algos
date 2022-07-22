12deeb42c076f88528eb105a054636bf453bc03a
Carlos Calderon <cdcalderon@gmail.com>
2022-07-21 20:22:17 -0500

    implement linked list

--- circular/linkedlist.js
+++ circular/linkedlist.js
@@ -1,5 +1,111 @@
-class Node {}
+class Node {
+    constructor(data, next = null) {
+        this.data = data
+        this.next = next
+    }
+}
 
-class LinkedList {}
+class LinkedList {
+    constructor(values = []) {
+        this.head = null
+
+        for (let value of values) {
+            this.insertLast(value)
+        }
+    }
+
+    clear() {
+        this.head = null
+    }
+
+    size() {
+        let counter = 0
+        let node = this.head
+
+        while (node) {
+            counter++
+            node = node.next
+        }
+
+        return counter
+    }
+
+    getAt(index) {
+        if (!this.head) {
+            return null
+        }
+
+        let counter = 0
+        let node = this.head
+        while (node) {
+            if (counter === index) {
+                return node
+            }
+            node = node.next
+            counter++
+        }
+        return null
+    }
+
+    insertAt(data, index) {
+        if (!this.head) {
+            this.head = new Node(data)
+            return
+        }
+
+        if (index === 0) {
+            this.head = new Node(data, this.head)
+            return
+        }
+
+        let counter = 1
+        let previous = this.head
+        let node = this.head.next
+        while (node) {
+            if (counter === index) {
+                previous.next = new Node(data, node)
+                return
+            }
+            previous = node
+            node = node.next
+            counter++
+        }
+
+        previous.next = new Node(data, node)
+    }
+
+    getFirst() {
+        return this.head
+    }
+
+    insertFirst(data) {
+        this.head = new Node(data, this.getFirst())
+    }
+
+    getLast() {
+        if (!this.head) {
+            return null
+        }
+
+        let node = this.head
+        while (node.next) {
+            node = node.next
+        }
+
+        return node
+    }
+
+    insertLast(data) {
+        const last = this.getLast()
+
+        if (last) {
+            last.next = new Node(data)
+            return last.next
+        } else {
+            this.head = new Node(data)
+            return this.head
+        }
+    }
+}
 
 module.exports = { Node, LinkedList }
