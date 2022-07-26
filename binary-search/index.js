3b6807fcb85b5e9cf2a7e9170453d3005475ac13
Carlos Calderon <cdcalderon@gmail.com>
2022-07-26 18:55:09 -0500

    add binary algo problem

---
+++ binary-search/index.js
@@ -0,0 +1,11 @@
+// --- Directions
+// We are given an array of integers, nums, sorted in ascending order,
+// and an integer value, target. If the target exists in the array, return its index. If the target does not exist, then return -1.
+
+// The binary search divides the input array by half at every step.
+// After every step, either we find the index we are looking for,
+// or we discard half of the array.
+
+function binarySearch(nums, target) {}
+
+module.exports = binarySearch
