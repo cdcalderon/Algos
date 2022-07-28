90635e1d6b24f62c5323034e98b86eb503fd3c15
Carlos Calderon <cdcalderon@gmail.com>
2022-07-28 18:57:53 -0500

    implement solution for bu=inary serach algo problem

--- binary-search/index.js
+++ binary-search/index.js
@@ -6,6 +6,30 @@
 // After every step, either we find the index we are looking for,
 // or we discard half of the array.
 
-function binarySearch(nums, target) {}
+function binarySearch(nums, target) {
+    let low = 0
+    let high = nums.length - 1
+
+    while (low <= high) {
+        // Finding the mid using floor division
+        let mid = low + Math.floor((high - low) / 2)
+
+        //Target value is present at the middle of the array
+        if (nums[mid] === target) {
+            return mid
+        }
+
+        //Target value is present in the low subarray
+        if (target < nums[mid]) {
+            high = mid - 1
+        }
+        //Target value is present in the high subarray
+        else {
+            low = mid + 1
+        }
+    }
+    // Target value is not present in the array
+    return -1
+}
 
 module.exports = binarySearch
