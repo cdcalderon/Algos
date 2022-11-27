be87aa80073e60cd1e3c31c6b29fca4a9591ac29
Carlos Calderon <cdcalderon@gmail.com>
2022-11-26 20:50:27 -0600

    implement fibo solution 2

--- fib/index.js
+++ fib/index.js
@@ -7,6 +7,7 @@
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
+
 function memoize(fn) {
     const cache = {}
     return function (...args) {
@@ -21,17 +22,27 @@ function memoize(fn) {
     }
 }
 
-function fib(n) {
-    const result = [0, 1]
-
-    for (let i = 2; i <= n; i++) {
-        const a = result[i - 1]
-        const b = result[i - 2]
-
-        result.push(a + b)
+function slowFib(n) {
+    if (n < 2) {
+        return n
     }
 
-    return result[n]
+    return fib(n - 1) + fib(n - 2)
 }
 
+const fib = memoize(slowFib)
+
 module.exports = fib
+
+// function fib(n) {
+//   const result = [0, 1];
+//
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+//
+//     result.push(a + b);
+//   }
+//
+//   return result[n];
+// }
