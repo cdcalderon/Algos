4cc12b09ea63d7a3549b833cf3ea72d16b6e46e7
Carlos Calderon <cdcalderon@gmail.com>
2022-11-30 20:16:50 -0600

    implement twoSumSolutionTwo

--- two-sum/index.js
+++ two-sum/index.js
@@ -6,6 +6,23 @@
 //You can return the answer in any order.
 
 function twoSum(nums, target) {
+    return twoSumSolutionTwo(nums, target)
+}
+
+function twoSumSolutionTwo(nums, target) {
+    for (let p1 = 0; p1 < nums.length; p1++) {
+        const numberToFind = target - nums[p1]
+        for (let p2 = 1; p2 < nums.length; p2++) {
+            if (numberToFind === nums[p2]) {
+                return [p1, p2]
+            }
+        }
+    }
+
+    return null
+}
+
+function twoSumSolutionOne(nums, target) {
     const numsMap = {}
 
     for (let p = 0; p < nums.length; p++) {
