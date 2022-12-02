b9ee093ae809d2aedcf6c67624d4318cb4cfa204
Carlos Calderon <cdcalderon@gmail.com>
2022-12-01 22:09:09 -0600

    implement brute force most water algorithm

--- most-water/index.js
+++ most-water/index.js
@@ -5,6 +5,26 @@
 
 //Return the maximum amount of water a container can store.
 
-function mostWater(nums, target) {}
+function mostWater(nums) {
+    return mostWaterSolutionOne(nums)
+}
+
+function mostWaterSolutionOne(nums) {
+    let maxValue = 0
+    for (let a = 0; a < nums.length; a++) {
+        for (let b = 1; b < nums.length; b++) {
+            const newArea = getArea(nums[a], nums[b], a, b)
+
+            maxValue = Math.max(maxValue, newArea)
+            console.log(maxValue)
+        }
+    }
+
+    return maxValue
+}
+
+function getArea(a, b, idxa, idxb) {
+    return Math.min(a, b) * (idxb - idxa)
+}
 
 module.exports = mostWater
