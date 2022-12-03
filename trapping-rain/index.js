d8f26739fa9c09873e14e53b3c7f49df6f6633b5
Carlos Calderon <cdcalderon@gmail.com>
2022-12-02 20:17:59 -0600

    implement trapping Water Solution Brute Force

--- trapping-rain/index.js
+++ trapping-rain/index.js
@@ -8,10 +8,36 @@ function trappingWater(nums) {
 
 function trappingWaterSolution(nums) {}
 
-function trappingWaterSolutionBruteForce(nums) {}
+function trappingWaterSolutionBruteForce(heights) {
+    let totalWater = 0
+    for (let p = 0; p < heights.length; p++) {
+        let pLeft = p
+        let pRight = p
+        let maxLeft = 0
+        let maxRight = 0
+
+        while (pLeft >= 0) {
+            maxLeft = Math.max(maxLeft, heights[pLeft])
+            pLeft--
+        }
+
+        while (pRight < heights.length) {
+            maxRight = Math.max(maxRight, heights[pRight])
+            pRight++
+        }
+
+        const currenWater = Math.min(maxLeft, maxRight) - heights[p]
+
+        if (currenWater >= 0) {
+            totalWater += currenWater
+        }
+    }
+
+    return totalWater
+}
 
 function getArea(a, b, idxa, idxb) {
     return Math.min(a, b) * (idxb - idxa)
 }
 
-module.exports = mostWater
+module.exports = trappingWater
