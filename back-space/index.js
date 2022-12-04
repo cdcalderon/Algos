33b3bf7a5446d64401ea3c2a8c155fffd1c15df8
Carlos Calderon <cdcalderon@gmail.com>
2022-12-04 13:52:23 -0600

    ass back space algo problem

---
+++ back-space/index.js
@@ -0,0 +1,8 @@
+// --- Directions
+// Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+// Note that after backspacing an empty text, the text will continue empty.
+
+function backspaceString(str1, str2) {}
+
+module.exports = typeOut
