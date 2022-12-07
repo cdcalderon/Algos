c03f0d133c1548611b81870c6dde32a612ad725b
Carlos Calderon <cdcalderon@gmail.com>
2022-12-06 20:55:59 -0600

    implement longest substring brute force solution

--- longest-substring/index.js
+++ longest-substring/index.js
@@ -1,6 +1,52 @@
 // --- Directions
 // Given a string s, find the length of the longest substring without repeating characters.
 
-function longestSubstring(str) {}
+function longestSubstring(str) {
+    return longestSubstringBruteForce(str)
+}
+
+// function longestSubstring(str) {
+//     if (str.length <= 1) return s.length
+
+//     let longest = 0
+//     for (let left = 0; left < str.length; left++) {
+//         let seenChar = {}
+//         let currentLength = 0
+//         for (let right = left; right < str.length; right++) {
+//             const currentChar = str[right]
+//             if (!seenChar[currentChar]) {
+//                 currentLength++
+//                 seenChar[currentChar] = true
+//                 longest = Math.max(longest, currentLength)
+//             } else {
+//                 break
+//             }
+//         }
+//     }
+
+//     return longest
+// }
+
+function longestSubstringBruteForce(str) {
+    let longestTotal = 0
+    let charMap = {}
+
+    for (let i = 0; i < str.length; i++) {
+        let currentLongest = 1
+        longestTotal = Math.max(currentLongest, longestTotal)
+        charMap[str[i]] = 1
+        for (let j = i + 1; j < str.length; j++) {
+            if (charMap[str[j]]) {
+                charMap = {}
+                break
+            } else {
+                currentLongest++
+                longestTotal = Math.max(currentLongest, longestTotal)
+                charMap[str[j]] = 1
+            }
+        }
+    }
+    return longestTotal
+}
 
 module.exports = longestSubstring
--- longest-substring/test.js
+++ longest-substring/test.js
@@ -5,13 +5,13 @@ test('maxChar function exists', () => {
 })
 
 test('Given a string abcabcbb, find the length of the longest substring without repeating characters should be 3', () => {
-    expect(longestSubstring('abcabcbb')).toEqual('3')
+    expect(longestSubstring('abcabcbb')).toEqual(3)
 })
 
 test('Given a string bbbbb, find the length of the longest substring without repeating characters should be 1', () => {
-    expect(longestSubstring('bbbbb')).toEqual('1')
+    expect(longestSubstring('bbbbb')).toEqual(1)
 })
 
 test('Given a string pwwkew, find the length of the longest substring without repeating characters should be 3.', () => {
-    expect(longestSubstring('pwwkew')).toEqual('3')
+    expect(longestSubstring('pwwkew')).toEqual(3)
 })
