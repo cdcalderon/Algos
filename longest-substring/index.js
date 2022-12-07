1dd682b63be95373494306b4498bbbcf84f59377
Carlos Calderon <cdcalderon@gmail.com>
2022-12-06 19:50:57 -0600

    add longest substring problem

---
+++ longest-substring/index.js
@@ -0,0 +1,6 @@
+// --- Directions
+// Given a string s, find the length of the longest substring without repeating characters.
+
+function longestSubstring(str) {}
+
+module.exports = longestSubstring
