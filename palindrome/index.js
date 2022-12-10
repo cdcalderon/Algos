76620b939ea5ef8a68226cda76194df0616b2d98
Carlos Calderon <cdcalderon@gmail.com>
2022-12-10 12:18:58 -0600

    implement another palindrome  solution

--- palindrome/index.js
+++ palindrome/index.js
@@ -8,13 +8,34 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-    return palindromeSolution1(str)
+    return palindromeSolutionNoHelpers(str)
 }
 
 function palindromeSolution1(str) {
     const reversed = reverse(str)
 
     return str == reversed
+}
+
+function palindromeSolutionNoHelpers(str) {
+    if (str.length === 1) {
+        return true
+    }
+    //str = str.replace(/[^\w]/g, '').trim()
+    // str = str.replace(/[^A-Za-z0-9 ]/g, '').trim()
+    //str = str.split('').reverse().join('')
+    let left = 0
+    let right = str.length - 1
+
+    while (left < right) {
+        console.log('testcc ', left, right)
+        if (str[left] !== str[right]) {
+            return false
+        }
+        left++
+        right--
+    }
+    return true
 }
 
 function reverse(inputString) {
