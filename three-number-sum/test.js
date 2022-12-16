b7df42af259135165b9ecfdc35d2b2712db4da63
Carlos Calderon <cdcalderon@gmail.com>
2022-12-15 18:50:42 -0600

    fix tests

--- three-number-sum/test.js
+++ three-number-sum/test.js
@@ -7,9 +7,7 @@ test('function threeNumberSum exists', () => {
 test('Write a function that takes in a non-empty array [12, 3, 1, 2, -6, 5, -8, 6] of distinct integers and an integer representing a target sum 0.', () => {
     const arr = [12, 3, 1, 2, -6, 5, -8, 6]
     const targetSum = 0
-    const threeNumberSumResult = threeNumberSum(arr, targetSum)
-    console.log('carlos', JSON.parse(threeNumberSumResult))
-    expect(threeNumberSumResult).toBe([
+    expect(threeNumberSum(arr, targetSum)).toEqual([
         [-8, 2, 6],
         [-8, 3, 5],
         [-6, 1, 5],
@@ -19,7 +17,6 @@ test('Write a function that takes in a non-empty array [12, 3, 1, 2, -6, 5, -8, 
 test('Write a function that takes in a non-empty array [1, 2, 3] of distinct integers and an integer representing a target sum 6.', () => {
     const arr = [1, 2, 3]
     const targetSum = 6
-    const threeNumberSumResult = threeNumberSum(arr, targetSum)
 
-    expect(threeNumberSumResult).toEqual([[1, 2, 3]])
+    expect(threeNumberSum(arr, targetSum)).toEqual([[1, 2, 3]])
 })
