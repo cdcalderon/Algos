ba695adb45e3b6f9aee98370dde9ef6a8b3a282d
Carlos Calderon <cdcalderon@gmail.com>
2022-12-15 21:12:29 -0600

    add unit tests

--- smallest-difference/test.js
+++ smallest-difference/test.js
@@ -10,3 +10,17 @@ test('Write a function that takes in two non-empty arrays of integers, finds the
 
     expect(smallestDifference(arr1, arr2)).toEqual([28, 26])
 })
+
+test('Write a function that takes in two non-empty arrays of integers, finds the pair of numbers (one from each array) whose absolute difference is closest to zero', () => {
+    const arr1 = [-1, 5, 10, 20, 3]
+    const arr2 = [26, 134, 135, 15, 17]
+
+    expect(smallestDifference(arr1, arr2)).toEqual([20, 17])
+})
+
+test('Write a function that takes in two non-empty arrays of integers, finds the pair of numbers (one from each array) whose absolute difference is closest to zero', () => {
+    const arr1 = [10, 0, 20, 25]
+    const arr2 = [1005, 1006, 1014, 1032, 1031]
+
+    expect(smallestDifference(arr1, arr2)).toEqual([25, 1005])
+})
