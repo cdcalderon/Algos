165ec16fc36793b3e9c3e70174af2c8e2757194c
Carlos Calderon <cdcalderon@gmail.com>
2022-12-20 14:54:38 -0600

    add unit tests

---
+++ monotonic-array/test.js
@@ -0,0 +1,9 @@
+const monotonicArray = require('./index')
+
+test('MonotonicArray is a function', () => {
+    expect(typeof monotonicArray).toEqual('function')
+})
+
+test('Given a function that takes in an array [-1, -5, -10, -1100, -1100, -1101, -1102, -9001] of integers and returns a boolean representing whether the array is monotonic.', () => {
+    expect(monotonicArray([-1, -5, -10, -1100, -1100, -1101, -1102, -9001])).toEqual(true)
+})
