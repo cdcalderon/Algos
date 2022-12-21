801b03505f46700548ed87554756bff7a5cbdb72
Carlos Calderon <cdcalderon@gmail.com>
2022-12-21 15:12:51 -0600

    add unit tests

--- monotonic-array/test.js
+++ monotonic-array/test.js
@@ -7,3 +7,15 @@ test('MonotonicArray is a function', () => {
 test('Given a function that takes in an array [-1, -5, -10, -1100, -1100, -1101, -1102, -9001] of integers and returns a boolean representing whether the array is monotonic.', () => {
     expect(monotonicArray([-1, -5, -10, -1100, -1100, -1101, -1102, -9001])).toEqual(true)
 })
+
+test('Given a function that takes in an array [] of integers and returns a boolean representing whether the array is monotonic.', () => {
+    expect(monotonicArray([])).toEqual(true)
+})
+
+test('Given a function that takes in an array [2, 1] of integers and returns a boolean representing whether the array is monotonic.', () => {
+    expect(monotonicArray([2, 1])).toEqual(true)
+})
+
+test('Given a function that takes in an array [-1, -5, -10, -1100, -1101, -1102, -9001] of integers and returns a boolean representing whether the array is monotonic.', () => {
+    expect(monotonicArray([-1, -5, -10, -1100, -1101, -1102, -9001])).toEqual(true)
+})
