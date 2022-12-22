059215ff49c4bb8bad38648e31d113fc106b9f0f
Carlos Calderon <cdcalderon@gmail.com>
2022-12-22 17:42:55 -0600

    add duplicate value problem

---
+++ first-duplicate-value/index.js
@@ -0,0 +1,12 @@
+// --- Directions
+/*
+    Given an array of integers between 1 and n, inclusive, where{' '}
+    n is the length of the array, write a function that returns the first integer that
+    appears more than once (when the array is read from left to right).
+*/
+
+function firstDuplicateValue(array) {
+    return 2
+}
+
+module.exports = firstDuplicateValue
