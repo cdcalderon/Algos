2bf196f2045032aba343e7d9ef982909dd34f5ef
Carlos Calderon <cdcalderon@gmail.com>
2022-12-22 18:56:39 -0600

    add runLengthEncoding algo problem

---
+++ run-length-encoding/index.js
@@ -0,0 +1,8 @@
+// --- Directions
+/*Write a function that takes in a 
+  non-empty string and returns its run-length encoding.
+*/
+
+function runLengthEncoding(string) {}
+
+module.exports = runLengthEncoding
