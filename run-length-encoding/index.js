c7e964aeae360be20a679f4592d21fa3812fb9ff
Carlos Calderon <cdcalderon@gmail.com>
2022-12-23 19:07:48 -0600

    implement runLengthEncoding solution

--- run-length-encoding/index.js
+++ run-length-encoding/index.js
@@ -3,6 +3,24 @@
   non-empty string and returns its run-length encoding.
 */
 
-function runLengthEncoding(string) {}
+function runLengthEncoding(string) {
+    let currentRunLength = 1
+    let encodedCharactersArray = []
+
+    for (let i = 1; i < string.length; i++) {
+        const currentCharacter = string[i]
+        const previousCharacter = string[i - 1]
+
+        if (currentCharacter !== previousCharacter || currentRunLength === 9) {
+            encodedCharactersArray.push(`${currentRunLength}${previousCharacter}`)
+            currentRunLength = 0
+        }
+
+        currentRunLength++
+    }
+
+    encodedCharactersArray.push(`${currentRunLength}${string[string.length - 1]}`)
+    return encodedCharactersArray.join('')
+}
 
 module.exports = runLengthEncoding
