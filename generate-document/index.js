c8b6c7a20cc03e2fbd7d4588e1719fea90f7570a
carlos calderon <ccalderon@yprime.com>
2022-12-29 23:24:10 -0600

    implement generateDocument solution

--- generate-document/index.js
+++ generate-document/index.js
@@ -7,6 +7,30 @@
   should return false.
 */
 
-function generateDocument(array) {}
+function generateDocument(characters, document) {
+    let charactersMap = getMapChars(characters)
+    let documentMap = getMapChars(document)
+
+    for (let key in documentMap) {
+        let charsCount = charactersMap[key]
+        let docsCount = documentMap[key]
+
+        if (!charsCount || docsCount > charsCount) return false
+    }
+    return true
+}
+
+function getMapChars(characters) {
+    let mapArray = {}
+
+    for (let i = 0; i < characters.length; i++) {
+        if (!mapArray[characters[i]]) {
+            mapArray[characters[i]] = 1
+        } else {
+            mapArray[characters[i]]++
+        }
+    }
+    return mapArray
+}
 
 module.exports = generateDocument
