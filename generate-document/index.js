5087d381437018e2c61f24a7f54f1497a639a124
carlos calderon <ccalderon@yprime.com>
2022-12-29 23:12:14 -0600

    add generateDocument algo problem

---
+++ generate-document/index.js
@@ -0,0 +1,12 @@
+// --- Directions
+/*
+  You're given a string of available characters and a string representing a
+  document that you need to generate. Write a function that determines if you
+  can generate the document using the available characters. If you can generate
+  the document, your function should return <span>true</span>; otherwise, it
+  should return false.
+*/
+
+function generateDocument(array) {}
+
+module.exports = generateDocument
