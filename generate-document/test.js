d4436007bb65da128b011bd90ab2737c5de0c083
carlos calderon <ccalderon@yprime.com>
2022-12-29 23:23:39 -0600

    fix test

--- generate-document/test.js
+++ generate-document/test.js
@@ -1,11 +1,9 @@
 const generateDocument = require('./index')
 
-test('arrayOfProducts is a function', () => {
+test('generateDocument is a function', () => {
     expect(typeof generateDocument).toEqual('function')
 })
 
-test(`Given a string 'a hsgalhsa sanbjksbdkjba kjx' and document 'AlgoExpert is the Best!', should return true`, () => {
-    expect(generateDocument('a hsgalhsa sanbjksbdkjba kjx', 'AlgoExpert is the Best!').toEqual(true)
+test(`Given a string 'Bste!hetsi ogEAxpelrt x ' and document 'AlgoExpert is the Best!', should return true`, () => {
+    expect(generateDocument('Bste!hetsi ogEAxpelrt x ', 'AlgoExpert is the Best!')).toEqual(true)
 })
-
-
