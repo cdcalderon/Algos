1b1fd7cb7585a1cb1460d7969d43aadd517b468e
Carlos Calderon <cdcalderon@gmail.com>
2023-02-02 00:01:10 -0600

    implement zeroSumSubarray2

--- zero-sum-subarray/index.js
+++ zero-sum-subarray/index.js
@@ -6,6 +6,10 @@
 */
 
 function zeroSumSubarray(nums) {
+    return zeroSumSubarray2(nums)
+}
+
+function zeroSumSubarray1(nums) {
     const sums = new Set([0])
     let currentSum = 0
 
@@ -17,6 +21,28 @@ function zeroSumSubarray(nums) {
         sums.add(currentSum)
     }
 
+    return false
+}
+
+function zeroSumSubarray2(nums) {
+    const sortedNums = nums.sort((a, b) => a - b)
+    let left = 0
+    let right = nums.length - 1
+
+    while (left < right) {
+        const currentSum = sortedNums[left] + sortedNums[right]
+
+        if (currentSum === 0) {
+            console.log('zero Sum -> : ', sortedNums[left], sortedNums[right])
+            return true
+        }
+
+        if (currentSum > 0) {
+            right--
+        } else {
+            left++
+        }
+    }
     return false
 }
 
