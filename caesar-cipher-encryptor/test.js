930e3fab5bb8eca9c6cb583b8633d6d8acd9e24f
Carlos Calderon <cdcalderon@gmail.com>
2023-02-02 23:19:50 -0600

    add unit tests

--- caesar-cipher-encryptor/test.js
+++ caesar-cipher-encryptor/test.js
@@ -11,3 +11,7 @@ test(`Given a string 'xyz' and key 2, should return 'zab'`, () => {
 test(`Given a string 'iwufqnkqkqoolxzzlzihqfm' and key 25, should return 'hvtepmjpjpnnkwyykyhgpel'`, () => {
     expect(caesarCipherEncryptor('iwufqnkqkqoolxzzlzihqfm', 25)).toEqual('hvtepmjpjpnnkwyykyhgpel')
 })
+
+test(`Given a string 'ovmqkwtujqmfkao' and key 52, should return 'ovmqkwtujqmfkao'`, () => {
+    expect(caesarCipherEncryptor('ovmqkwtujqmfkao', 52)).toEqual('ovmqkwtujqmfkao')
+})
--- zero-sum-subarray/test.js
+++ zero-sum-subarray/test.js
@@ -4,6 +4,14 @@ test('zeroSumSubarray is a function', () => {
     expect(typeof zeroSumSubarray).toEqual('function')
 })
 
-test(`Given a list of integers nums, should return true`, () => {
+test(`Given a list of integers nums [1, 2, 3], should return true`, () => {
     expect(zeroSumSubarray([1, 2, 3])).toEqual(false)
+})
+
+test(`Given a list of integers nums [-1, -1, -1], should return true`, () => {
+    expect(zeroSumSubarray([-1, -1, -1])).toEqual(false)
+})
+
+test(`Given a list of integers nums [-1, 2, 3, 4, -5, -3, 1, 2], should return true`, () => {
+    expect(zeroSumSubarray([-1, 2, 3, 4, -5, -3, 1, 2])).toEqual(true)
 })
