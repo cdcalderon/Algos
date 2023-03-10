8a649f62ac6c97c33093304126d164aac19b94bc
carlos calderon <cdcalderon@gmail.com>
2023-03-09 19:52:04 -0600

    implement duplicateZeros algorithm

--- duplicate-zeros/index.js
+++ duplicate-zeros/index.js
@@ -3,6 +3,32 @@
 
 // Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
 
-function duplicateZeros(n) {}
+function duplicateZeros(arr) {
+    const length = arr.length - 1
+    let w = length
+
+    for (let r = length - getNumberOfZeros(arr); r >= 0; r--) {
+        if (arr[r] === 0) {
+            arr[w] = 0
+            w--
+            arr[w] = 0
+        } else {
+            arr[w] = arr[r]
+        }
+        w--
+    }
+    console.log(`carlos -- > ${arr}`)
+    return arr
+}
+
+function getNumberOfZeros(arr) {
+    let zerosCount = 0
+    for (let i = 0; i < arr.length - zerosCount; i++) {
+        if (arr[i] === 0) {
+            zerosCount++
+        }
+    }
+    return zerosCount
+}
 
 module.exports = duplicateZeros
--- duplicate-zeros/test.js
+++ duplicate-zeros/test.js
@@ -3,3 +3,7 @@ const duplicateZeros = require('./index')
 test(`duplicateZeros function is defined`, () => {
     expect(duplicateZeros).toBeDefined()
 })
+
+test('Given a fixed-length integer array [1,0,2,3,0,4,5,0], it should return [1,0,0,2,3,0,0,4]', () => {
+    expect(duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0])).toEqual([1, 0, 0, 2, 3, 0, 0, 4])
+})
