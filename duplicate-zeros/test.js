5b0cf157ff0ccafdee6d3c8b743dde4bcc6d8ea5
carlos calderon <cdcalderon@gmail.com>
2023-03-09 19:53:40 -0600

    add unit tests

--- duplicate-zeros/test.js
+++ duplicate-zeros/test.js
@@ -7,3 +7,7 @@ test(`duplicateZeros function is defined`, () => {
 test('Given a fixed-length integer array [1,0,2,3,0,4,5,0], it should return [1,0,0,2,3,0,0,4]', () => {
     expect(duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0])).toEqual([1, 0, 0, 2, 3, 0, 0, 4])
 })
+
+test('Given a fixed-length integer array [1,2,3], it should return [1,2,3]', () => {
+    expect(duplicateZeros([1, 2, 3])).toEqual([1, 2, 3])
+})
