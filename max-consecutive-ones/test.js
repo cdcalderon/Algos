0ad78b037395eb26cc0bfd84ed02001d7d112576
carlos calderon <cdcalderon@gmail.com>
2023-03-20 13:08:05 -0500

    add unit tests

--- max-consecutive-ones/test.js
+++ max-consecutive-ones/test.js
@@ -1,5 +1,13 @@
 const maxConsecutiveOnes = require('./index')
 
-test('maxConsecutiveOnes function exists', () => {
-    expect(typeof maxConsecutiveOnes).toEqual('function')
+test(`Given a binary array [1,1,0,1,1,1], return should be 3`, () => {
+    expect(maxConsecutiveOnes([1, 1, 0, 1, 1, 1])).toEqual(3)
+})
+
+test(`Given a binary array [1, 1, 0, 1, 1, 1, 1, 0, 1, 0], return should be 4`, () => {
+    expect(maxConsecutiveOnes([1, 1, 0, 1, 1, 1, 1, 0, 1, 0])).toEqual(4)
+})
+
+test(`Given a binary array [1, 1, 0, 1, 0, 1, 0, 0, 1, 0], return should be 3`, () => {
+    expect(maxConsecutiveOnes([1, 1, 0, 1, 0, 1, 0, 0, 1, 0])).toEqual(2)
 })
