e7d6b0da9b9dec57726f78ba7acc2b02098ae932
carlos calderon <cdcalderon@gmail.com>
2023-03-20 13:07:35 -0500

    implement solution maxConsecutiveOnes

--- max-consecutive-ones/index.js
+++ max-consecutive-ones/index.js
@@ -1,7 +1,28 @@
 // --- Directions
 // Given a binary array nums, return the maximum number of consecutive 1's in the array.
 
-function maxConsecutiveOnes(str) {
+function maxConsecutiveOnes(binaryArray) {
+    let start = 0
+    let end = 0
+    let maxConsecutiveOnes = 0
+
+    while (end < binaryArray.length) {
+        if (binaryArray[end] === 0 || end === binaryArray.length - 1) {
+            let currentMaxLength = 0
+            if (end === binaryArray.length - 1) {
+                currentMaxLength = end - start + 1
+            } else {
+                currentMaxLength = end - start
+            }
+
+            maxConsecutiveOnes = Math.max(maxConsecutiveOnes, currentMaxLength)
+            end++
+            start = end
+            continue
+        }
+        end++
+    }
+
     return maxConsecutiveOnes
 }
 
