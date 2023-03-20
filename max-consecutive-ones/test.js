9c2fbac17be674256af5bc443caa2a560bc77067
carlos calderon <cdcalderon@gmail.com>
2023-03-20 13:09:25 -0500

    fix / add unit test

--- max-consecutive-ones/test.js
+++ max-consecutive-ones/test.js
@@ -8,6 +8,10 @@ test(`Given a binary array [1, 1, 0, 1, 1, 1, 1, 0, 1, 0], return should be 4`, 
     expect(maxConsecutiveOnes([1, 1, 0, 1, 1, 1, 1, 0, 1, 0])).toEqual(4)
 })
 
-test(`Given a binary array [1, 1, 0, 1, 0, 1, 0, 0, 1, 0], return should be 3`, () => {
+test(`Given a binary array [1, 1, 0, 1, 0, 1, 0, 0, 1, 0], return should be 2`, () => {
     expect(maxConsecutiveOnes([1, 1, 0, 1, 0, 1, 0, 0, 1, 0])).toEqual(2)
+})
+
+test(`Given a binary array [1,0,1,1,0,1], return should be 2`, () => {
+    expect(maxConsecutiveOnes([1, 0, 1, 1, 0, 1])).toEqual(2)
 })
